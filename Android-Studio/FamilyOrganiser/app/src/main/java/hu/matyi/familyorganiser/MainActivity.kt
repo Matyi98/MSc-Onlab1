22747dad53059c607cb4c53d6ae82bd448682412
Active-hub <61094345+Active-hub@users.noreply.github.com>
2022-02-22 18:00:02 +0100

    Login form elkészítése (1) béta verzió

--- Android-Studio/FamilyOrganiser/app/src/main/java/hu/matyi/familyorganiser/MainActivity.kt
+++ Android-Studio/FamilyOrganiser/app/src/main/java/hu/matyi/familyorganiser/MainActivity.kt
@@ -5,111 +5,120 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import hu.matyi.familyorganiser.ui.theme.FamilyOrganiserTheme
-
-
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.*
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.toColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FamilyOrganiserTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-                Surface(color = MaterialTheme.colors.background) {
-                    // at below line we are calling
-                    // our function for button.
-                    MyButton()
-                }
+                SignInScreen()
+            }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+   @Composable
+   fun SignInScreen() {
+       Scaffold(backgroundColor = Color.White)
+       {
+           Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top)
+           {
+                Image(painter = painterResource(
+                    id = R.drawable.family),
+                    contentDescription = "App Theme",
+                    modifier = Modifier
+                        .weight(1f)
+                )
+               Card(
+                   Modifier
+                       .weight(2f)
+                       .padding(8.dp),
+                   shape = RoundedCornerShape(32.dp)
+               )
+               {
+                   Column(
+                       Modifier
+                           .background(Color(R.color.teal_700))
+                           .fillMaxSize()
+                           .padding(32.dp)) {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FamilyOrganiserTheme {
-        Greeting("Android")
-        MyButton()
+                       Text(text = "Welcome Back!",
+                           fontWeight = FontWeight.Bold,
+                           fontSize =  25.sp,
+                           modifier = Modifier.fillMaxWidth().padding(20.dp).fillMaxWidth(),
+                           color = Color.White)
+                       TextField(
+                           value ="" ,
+                           onValueChange = {},
+                           modifier = Modifier.fillMaxWidth().padding(20.dp).fillMaxWidth(),
+                           label = {Text(text = "Username", fontSize = 25.sp)},
+                       trailingIcon =
+                       {IconButton(onClick = {})
+                       {
+                           Icon(imageVector = Icons.Filled.Clear, contentDescription = "")
+                       }
+                       })
+                       TextField(
+                           value ="" ,
+                           onValueChange = {},
+                           modifier = Modifier.fillMaxWidth().padding(20.dp),
+                           label = {Text(text = "Password", fontSize = 25.sp)},
+                           trailingIcon =
+                           {IconButton(onClick = {})
+                           {
+                               Icon(imageVector = Icons.Filled.Clear, contentDescription = "")
+                           }
+                           })
+                       Button(
+                           onClick = {
+                           },
+                           modifier = Modifier.fillMaxWidth().padding(20.dp),
+                           enabled = true,
+                           border = BorderStroke(width = 1.dp, brush = SolidColor(Color.Blue)),
+                           shape = MaterialTheme.shapes.medium,
+                       )
+                       {
+                           Text(text = "Log in", color = Color.White)
+                       }
+                       Button(
+                           onClick = {
+                           },
+                           modifier = Modifier.fillMaxWidth().padding(20.dp),
+                           enabled = true,
+                           border = BorderStroke(width = 1.dp, brush = SolidColor(Color.Blue)),
+                           shape = MaterialTheme.shapes.medium,
+                       )
+                       {
+                           Text(text = "Not yet registered?", color = Color.White)
+                       }
 
-    }
-}
-
-    @Composable
-    fun MyButton() {
-
-        Column(
-            // we are using column to align our
-            // imageview to center of the screen.
-            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
-
-            // below line is used for specifying
-            // vertical arrangement.
-            verticalArrangement = Arrangement.Center,
-
-            // below line is used for specifying
-            // horizontal arrangement.
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            // below line is use to get
-            // the context for our app.
-            val context = LocalContext.current
-
-            // below line is use to create a button.
-            Button(
-                // below line is use to add onclick
-                // parameter for our button onclick
-                onClick = {
-                    // when user is clicking the button
-                    // we are displaying a toast message.
-                    Toast.makeText(context, "Welcome to Geeks for Geeks", Toast.LENGTH_LONG).show()
-                },
-                // in below line we are using modifier
-                // which is use to add padding to our button
-                modifier = Modifier.padding(all = Dp(10F)),
-
-                // below line is use to set or
-                // button as enable or disable.
-                enabled = true,
-
-                // below line is use to
-                // add border to our button.
-                border = BorderStroke(width = 1.dp, brush = SolidColor(Color.Blue)),
-
-                // below line is use to add shape for our button.
-                shape = MaterialTheme.shapes.medium,
-            )
-            // below line is use to
-            // add text on our button
-            {
-                Text(text = "Geeks for Geeks", color = Color.White)
-            }
-        }
-    }
+                   }
+               }
+           }
+       }
+   }
 
 
